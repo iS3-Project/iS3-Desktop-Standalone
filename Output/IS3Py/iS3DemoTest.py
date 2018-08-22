6c7609414ceb7141b4c1294ef1a03818f3250e23
iS3-Project <xiyue2088@126.com>
2018-08-22 13:15:09 +0800

    revise data

--- Output/IS3Py/iS3DemoTest.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:gb2312 -*-
-import is3
-is3.mainframe.LoadProject('iS3DemoTest.xml')
-is3.prj = is3.mainframe.prj
-is3.MainframeWrapper.loadDomainPanels()
-for emap in is3.prj.projDef.EngineeringMaps:
-    is3.MainframeWrapper.addView(emap)
-is3.addView3d('Map3D', 'iS3DemoTest.unity3d')
